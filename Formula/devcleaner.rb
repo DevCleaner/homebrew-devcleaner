5cb8737e4f80fdf520bbb0a3cc822803c8c89dc4
yacosta738 <yunielacosta738@gmail.com>
2021-12-16 01:48:36 +0000

    Update formula for devcleaner release v0.1.0

--- Formula/devcleaner.rb
+++ Formula/devcleaner.rb
@@ -2,14 +2,14 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 class DevCleaner < Formula
-  desc "A fast and simple dashboard for Kubernetes written in Rust"
+  desc "A tool to search for node_modules an clean the projects selected"
   homepage "https://github.com/DevCleaner/devcleaner"
   if OS.mac?
     url "https://github.com/DevCleaner/devcleaner/releases/download/v0.1.0/devcleaner-macos.tar.gz"
-    sha256 "a939ba93aea7e0575c4e2d3fdfd697b98c9bb14f062c169a0913e5e35660886e"
+    sha256 "b2a183a825efb7ab8159c152c2bdd9d31e2f26ece0033d88a53ea17e68fa6e28"
   else
     url "https://github.com/DevCleaner/devcleaner/releases/download/v0.1.0/devcleaner-linux-musl.tar.gz"
-    sha256 "c36804520a321aa1ddf396b2f8f188a7c90c62900b70e14536deb220988e8f65"
+    sha256 "680d1a5c839df841e33b950ef1f4e528e2b0fa68e14c4a11d23b1a565a197c4f"
   end
   version "v0.1.0"
   license "MIT"
